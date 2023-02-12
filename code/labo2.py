0955025c638922fb03d0f04ac6ba31fe5c422dc7
Jacty <saej3101@usherbrooke.ca>
2023-02-12 11:46:32 -0500

    Fix labo 2

--- code/labo2.py
+++ code/labo2.py
@@ -2,28 +2,23 @@ import math
 
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy import signal
 
 
 def rep_impulsionnelle(fc, fe, N):
     n = np.arange(0 - (N / 2), (N / 2))
     m = N * (fc / fe)
-    k = 2 * m + 1
-    add_padding = False
-    padding = 0 if add_padding is False else 50
+    k = (2 * m) + 1
+    padding = 40000
 
     # Calcul reponse impulsionnelle
     h = [1 / N * (np.sin(math.pi * n[i] * k / N) / np.sin(math.pi * n[i] / N)) if n[i] != 0 else k/N for i in range(N)]
     h_ham = np.hamming(N) * h
 
-    # fft, amplitude et phase
-    hfft = np.fft.fft(h)
+    h_zpad = np.append(h, np.zeros(padding))
+    hfft = np.fft.fft(h_zpad)
 
-    h_db = [20 * np.log10(np.abs(x)) if x != 0 else 0 for x in hfft]
-    w = [(i * 2 * math.pi) / (N + padding) for i in range(N + padding)]
-
-    if add_padding is True:
-        h_db.extend([0] * padding)
+    h_db = 20 * np.log10(np.abs(hfft)) 
+    w = [(i * 2 * math.pi) / (len(hfft)) for i in range(len(hfft))]
 
     # Representation
     plt.title(f"Réponse impulsionnelle h[n] (N={N})")
